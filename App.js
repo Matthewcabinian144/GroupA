dd5a63d2c4995280747d602a24adc30aaf126e06
ksuryalekha <77710056+ksuryalekha@users.noreply.github.com>
2021-01-23 10:06:17 +0800

    Update App.js
    updated app.js (works with mysql db)

--- App.js
+++ App.js
@@ -1,48 +1,46 @@
 import './App.css';
 import { Button, Tab, Tabs, Table, Form, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import axios from 'axios';
+//import DateTimePicker from 'react-datetime-picker';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-calendar/dist/Calendar.css';
+
 function App() {
   const [booking, setBooking] = useState([]);
-  useEffect(() => {
-    // Update the document title using the browser API
-    axios.get('http://localhost:5000/booking').then(res => {
-      console.log(res.data);
-      setBooking(res.data);
-    }).catch(error => {
-      console.log(error.response)
-      alert(JSON.stringify(error.response.data));
-    });
-
-  }, []);
+  const [value, onChange] = useState(new Date());
 
   const [name, setName] = useState('');
   const [nric, setNric] = useState('');
-  const [date, setDate] = useState('Choose...');
   const [category, setCategory] = useState('');
+  const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
 
   const formSubmit = (event) => {
     event.preventDefault();
     console.log(event);
     console.log("name", name);
     console.log("nric", nric);
-    console.log("date", date);
     console.log("category", category);
+    console.log("location", location);
+    console.log("date", date);
 
     axios.post('http://localhost:5000/booking', {
       name: name,
       nric: nric,
-      date: date,
-      category: category
-    }).then(res => {
+      category: category,
+      location: location,
+      date: "2021-01-21 07:00:00",
+
+    }).then(res => {  
       console.log(res.data);
       alert(JSON.stringify(res.data));
 
     }).catch(error => {
       console.log(error.response)
-      alert(JSON.stringify(error.response.data));
-    });
+         });
   }
 
   const deleteBooking = (event) => {
@@ -65,15 +63,16 @@ function App() {
     <div className="App">
       <h1> Covid19 Appointment Management System</h1>
       <Tabs defaultActiveKey="booking">
-        <Tab eventKey="booking" title="Home">
+        <Tab eventKey="booking" title="View Bookings">
           <h1>List of Booking</h1>
-          <Table striped bordered hover variant="dark">
+          <Table striped bordered hover variant="=outlined-dark"> 
             <thead>
               <tr>
                 <th>ID</th>
                 <th>Name</th>
                 <th>NRIC</th>
                 <th>Category</th>
+                <th>Location</th>
                 <th>Date</th>
               </tr>
             </thead>
@@ -84,15 +83,15 @@ function App() {
                   <td>{booking.name}</td>
                   <td>{booking.nric}</td>
                   <td>{booking.category}</td>
+                  <td>{booking.location}</td>
                   <td>{booking.date}</td>
-
                 </tr>
               })}
             </tbody>
           </Table>
         </Tab>
         <Tab eventKey="addbooking" title="Book Appointment">
-          <Card className="bg-light mx-auto" style={{ width: '30rem' }}>
+          <Card className="bg-light mx-auto" style={{ width: '40rem' }}>
             <Card.Header>Add Booking</Card.Header>
             <Form onSubmit={(event) => formSubmit(event)}>
               <Form.Group controlId="name">
@@ -117,13 +116,23 @@ function App() {
                 </Form.Control>
               </Form.Group>
 
+              <Form.Group controlId="location">
+                <Form.Label>Location</Form.Label>
+                <Form.Control as="select" value={location} onChange={e => setLocation(e.target.value)}>
+                  <option>Choose...</option>
+                  <option>Jurong</option>
+                  <option>Changi</option>
+                  <option>Punggol</option>
+                </Form.Control>
+              </Form.Group>
+
               <Form.Group controlId="date">
                 <Form.Label>Date</Form.Label>
-                {/* <DateTimePicker step={90} />
-                <DateTimePicker defaultValue={new Date()} />
-                <DateTimePicker min={new Date()} /> */}
-                <Form.Control placeholder="Choose Date.." value={date} onChange={e => setDate(e.target.value)} />
-              </Form.Group>
+                <DatePicker style="width:200px" selected={value} value={value} onChange={onChange} 
+                showTimeSelect timeFormat="HH:mm" timeCaption="time" 
+                timeIntervals={60} dateFormat="MMMM d, yyyy h:mm aa"/>  
+                </Form.Group>            
+                                
               <Button variant="primary" type="submit">Submit</Button>
             </Form>
           </Card>
@@ -133,6 +142,3 @@ function App() {
   );
 }
 export default App;
-
-
-
